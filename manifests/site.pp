403728ef171dda89c97bfa42cfa6e90b51c7a461
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-10-06 16:49:40 -0300

    Install Zookeeper before installing Kafka.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,13 +3,13 @@ Package {
   allow_virtual => false,
 }
 
-node /^ubuntu/ {
+node /ubuntu/ {
 
   file { 'bash_profile':
     path    => '/home/vagrant/.bash_profile',
     ensure  => file,
     source  => '/vagrant/manifests/bash_profile',
-    require => Class['cassandra']
+    before => Class['kafka']
   }
 
   exec { 'update-apt-packages':
@@ -20,20 +20,21 @@ node /^ubuntu/ {
   class { 'zookeeper':
   	client_ip => $::ipaddress_lo,
    	require => Exec['update-apt-packages'],
-	before => Class['kafka']
+	  before => Class['kafka']
   }
 
   # Configure a Kafka Broker
   class {'kafka':
-	version => $::kafka_version,
-	scala_version => $::scala_version,
-	require => Class['zookeeper'],
-	before => Class['kafka:broker']
+  	version => $::kafka_version,
+  	scala_version => $::scala_version,
+    install_dir => "/opt/kafka-bin",
+  	require => Class['zookeeper'],
+  	before => Class['kafka::broker']
   }
 
-  class {'kafka:broker':
-	config => { 'broker.id' => '0', 'zookeeper.connect' => 'localhost:2181' },
-	require => Class['kafka']
+  class {'kafka::broker':
+  	config => { 'broker.id' => '0', 'zookeeper.connect' => 'localhost:2181' },
+  	require => Class['kafka']
   }
 
   class { 'boundary':
@@ -47,7 +48,8 @@ node /^centos-7-0/ {
   file { 'bash_profile':
     path    => '/home/vagrant/.bash_profile',
     ensure  => file,
-    source  => '/vagrant/manifests/bash_profile'
+    source  => '/vagrant/manifests/bash_profile',
+    before => Class['kafka']
   }
 
   exec { 'update-rpm-packages':
@@ -57,6 +59,27 @@ node /^centos-7-0/ {
   package {'epel-release':
     ensure => 'installed',
     require => Exec['update-rpm-packages'],
+  }
+
+  # Install Zookeeper at 127.0.0.1
+  class { 'zookeeper':
+    client_ip => $::ipaddress_lo,
+    require => Exec['update-rpm-packages'],
+    before => Class['kafka']
+  }
+
+  # Configure a Kafka Broker
+  class {'kafka':
+    version => $::kafka_version,
+    scala_version => $::scala_version,
+    install_dir => "/opt/kafka-bin",
+    require => Class['zookeeper'],
+    before => Class['kafka::broker']
+  }
+
+  class {'kafka::broker':
+    config => { 'broker.id' => '0', 'zookeeper.connect' => 'localhost:2181' },
+    require => Class['kafka']
   }
 
 }
@@ -66,17 +89,38 @@ node /^centos/ {
   file { 'bash_profile':
     path    => '/home/vagrant/.bash_profile',
     ensure  => file,
-    source  => '/vagrant/manifests/bash_profile'
+    source  => '/vagrant/manifests/bash_profile',
+    before => Class['kafka']
   }
 
   exec { 'update-rpm-packages':
     command => '/usr/bin/yum update -y',
-    creates => '/vagrant/.locks/update-rpm-packages',
   }
 
   package {'epel-release':
     ensure => 'installed',
     require => Exec['update-rpm-packages'],
+  }
+
+  # Install Zookeeper at 127.0.0.1
+  class { 'zookeeper':
+    client_ip => $::ipaddress_lo,
+    require => Exec['update-rpm-packages'],
+    before => Class['kafka']
+  }
+
+  # Configure a Kafka Broker
+  class {'kafka':
+    version => $::kafka_version,
+    scala_version => $::scala_version,
+    install_dir => "/opt/kafka-bin",
+    require => Class['zookeeper'],
+    before => Class['kafka::broker']
+  }
+
+  class {'kafka::broker':
+    config => { 'broker.id' => '0', 'zookeeper.connect' => 'localhost:2181' },
+    require => Class['kafka']
   }
 
   class { 'boundary':
